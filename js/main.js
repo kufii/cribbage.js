7e2d8005be39667374fe741b411173bcc6057731
Adrien Pyke <adpyke@gmail.com>
2016-09-28 17:20:36 -0400

    enhanced example

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 	'use strict';
 
 	var cribbage = Cribbage({
-		canvas: document.querySelector('#cribbage')
+		canvas: document.querySelector('#cribbage'),
+		onwin: function(player) {
+			alert('Player ' + (player + 1) + ' wins!');
+			cribbage.disable();
+			cribbage.setTheme({
+				background: cribbage.getPegColor(player)
+			});
+		}
 	});
+
+	document.querySelector('#reset').onclick = function(e) {
+		cribbage.enable();
+		cribbage.setTheme();
+		cribbage.reset();
+	};
 })();
